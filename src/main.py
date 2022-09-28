28371ebc881997b1af7e5cf4be0d48c34c70d1cc
Juan Luis <juanluiscr27@hotmail.com>
2022-09-27 23:20:15 -0400

    Importing the list of stocks

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 # This is a Python script for an Equal-Weight S&P 500 Index Fund
+import screener
+
 
 def main():
-    print("")
+    print("S&P 500 Index Fund Screener")
+    screener.import_stocks()
 
 
 if __name__ == '__main__':
--- src/screener.py
+++ src/screener.py
@@ -4,3 +4,7 @@ import requests
 import xlsxwriter
 import math
 
+
+def import_stocks():
+    stocks = pd.read_csv('./data/sp_500_stocks.csv')
+    print(stocks)
