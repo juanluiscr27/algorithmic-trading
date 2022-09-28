aaea763fd0f5dc82f4ca179b912036dcd5aae2d7
Juan Luis <juanluiscr27@hotmail.com>
2022-09-28 18:52:41 -0400

    Acquiring an API Token

--- src/screener.py
+++ src/screener.py
@@ -3,8 +3,16 @@ import pandas as pd
 import requests
 import xlsxwriter
 import math
+from src.secrets import IEX_CLOUD_API_TOKEN
 
 
 def import_stocks():
     stocks = pd.read_csv('./data/sp_500_stocks.csv')
     print(stocks)
+
+
+def search_symbol(symbol=None):
+    if not symbol:
+        symbol = "AAPL"
+
+    api_url = f"https://sandbox.iexapis.com/stock/{symbol}/quote/"
