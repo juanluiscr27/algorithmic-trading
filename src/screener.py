6015915d34811fecab45e73ed71e6d477ae79ac8
Juan Luis <juanluiscr27@hotmail.com>
2022-10-02 12:58:50 -0400

    A chunk function for the list of stocks

--- src/screener.py
+++ src/screener.py
@@ -66,3 +66,10 @@ def get_data_all():
 
     print(df)
 
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
