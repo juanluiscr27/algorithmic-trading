993456d0871c7e245c0c1f1119d32d79c6855175
Juan Luis <juanluiscr27@hotmail.com>
2022-10-02 23:11:55 -0400

    Making a batch API call

--- src/screener.py
+++ src/screener.py
@@ -73,3 +73,32 @@ def chunks(lst, n):
         yield lst[i:i + n]
 
 
+def get_data_batch():
+    stocks = pd.read_csv('./data/sp_500_stocks.csv')
+    symbol_groups = chunks(stocks["Ticker"], 100)
+    symbol_strings = []
+    for index, symbol_group in enumerate(symbol_groups):
+        symbol_strings.append(",".join(symbol_group))
+
+    data_columns = ["Ticker", "Stock Price", "Market Capitalization", "Number of Shares to Buy"]
+    df = pd.DataFrame(columns=data_columns)
+    for symbol_str in symbol_strings:
+        batch_call_api_url = f"https://sandbox.iexapis.com/stable/stock/market/batch?symbols=" \
+                             f"{symbol_str}&types=quote&token={IEX_CLOUD_API_TOKEN}"
+        data = requests.get(batch_call_api_url).json()
+        for symbol in symbol_str.split(","):
+            df = pd.concat([
+                df,
+                pd.Series([
+                    symbol,
+                    data[symbol]["quote"]["latestPrice"],
+                    data[symbol]["quote"]["marketCap"],
+                    "NaN"
+                ],
+                    index=data_columns
+                ).to_frame().transpose()
+            ],
+                ignore_index=True
+            )
+
+    print(df)
