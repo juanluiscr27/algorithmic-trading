24dca348f32c957cb55e089adbcbb52830d12576
Juan Luis <juanluiscr27@hotmail.com>
2022-10-25 23:51:31 -0400

    import stocks for project 2

--- src/quantitative_momentum/quantitative_investing.py
+++ src/quantitative_momentum/quantitative_investing.py
@@ -1,4 +1,10 @@
 # This is a Python script for a Quantitative Momentum Investing Strategy
+import strategy as mtm
 
 
+def main():
+    mtm.import_stocks()
 
+
+if __name__ == "__main__":
+    main()
--- src/quantitative_momentum/strategy.py
+++ src/quantitative_momentum/strategy.py
@@ -1,6 +1,10 @@
 import pandas as pd
-import numpy as np
-import requests
-import math
-from scipy import stats
+# import numpy as np
+# import requests
+# import math
+# from scipy import stats
 
+
+def import_stocks():
+    stocks = pd.read_csv('./data/sp_500_stocks.csv')
+    print(stocks)
