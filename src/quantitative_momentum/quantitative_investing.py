49fdeb78b9af4d97b08d611adb0ee86179c4ecbb
Juan Luis <juanluiscr27@hotmail.com>
2022-10-26 21:47:41 -0400

    Get batch data for the one-year return

--- src/quantitative_momentum/quantitative_investing.py
+++ src/quantitative_momentum/quantitative_investing.py
@@ -5,7 +5,8 @@ import strategy as mtm
 def main():
     # mtm.import_stocks()
     # mtm.stats_api_call("AAPL")
-    mtm.get_change_pct("AAPL")
+    # mtm.get_change_pct("AAPL")
+    mtm.get_data_batch()
 
 
 if __name__ == "__main__":
--- src/quantitative_momentum/strategy.py
+++ src/quantitative_momentum/strategy.py
@@ -21,3 +21,40 @@ def stats_api_call(symbol):
 def get_change_pct(symbol):
     data = stats_api_call(symbol)
     print(data['year1ChangePercent'])
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+def get_data_batch():
+    stocks = pd.read_csv('./data/sp_500_stocks.csv')
+    symbol_groups = chunks(stocks["Ticker"], 100)
+    symbol_strings = []
+    for _, symbol_group in enumerate(symbol_groups):
+        symbol_strings.append(",".join(symbol_group))
+
+    data_columns = ["Ticker", "Price", "One-Year Price Return", "Number of Shares to Buy"]
+    df = pd.DataFrame(columns=data_columns)
+    for symbol_string in symbol_strings[:1]:
+        batch_api_call_url = f"https://sandbox.iexapis.com/stable/stock/market/batch?" \
+                        f"symbols={symbol_string}&types=price,stats&token={IEX_CLOUD_API_TOKEN}"
+        data = requests.get(batch_api_call_url).json()
+        for symbol in symbol_string.split(","):
+            df = pd.concat([
+                df,
+                pd.Series([
+                    symbol,
+                    data[symbol]["price"],
+                    data[symbol]["stats"]["year1ChangePercent"],
+                    "N/A"
+                ],
+                    index=data_columns
+                ).to_frame().T
+            ],
+                ignore_index=True
+            )
+
+    print(df)
